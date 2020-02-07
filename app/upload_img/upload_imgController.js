4777ccee57a8a6f51f30da54bfe66b8519c93a76
TheLastHope <puchit@windowslive.com>
2020-02-07 14:25:19 +0700

    upload img

--- app/upload_img/upload_imgController.js
+++ app/upload_img/upload_imgController.js
@@ -3,6 +3,9 @@ import upload_imgModel from "./upload_imgModel"
 const Upload_imgController = {
     async upload_img(req, res) {
         if (req.user) {
+            require("fs").writeFile(__dirname + "/store/1.png", req.body.editImage, 'base64', function(err) {
+                console.log(err);
+            });
             await upload_imgModel.upload_img(req.body)
             res.status(201).json({
                 "result": "success",
--- app/upload_img/upload_imgModel.js
+++ app/upload_img/upload_imgModel.js
@@ -6,7 +6,7 @@ const Upload_imgModel = {
     upload_img(req) {
         return new Promise((resolve, reject) => {
             let sql = "UPDATE employee SET employee_fname = ? ,employee_lname = ? ,employee_tel = ? , employee_image = ? WHERE employee_id = ?"
-            let query = mysql.format(sql, [req.editFname, req.editLname, req.editTel, req.editImage[0].objectURL.changingThisBreaksApplicationSecurity, req.id])
+            let query = mysql.format(sql, [req.editFname, req.editLname, req.editTel, req.id, req.id])
             connection().query(query, (err, result) => {
                 if (err) reject(err)
                 return resolve(result)

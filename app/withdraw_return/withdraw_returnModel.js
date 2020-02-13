e0ed65496157c24a7bfa81086d1b3c95dd3f7e80
TheLastHope <puchit@windowslive.com>
2020-02-13 21:30:37 +0700

    commit

--- app/withdraw_return/withdraw_returnModel.js
+++ app/withdraw_return/withdraw_returnModel.js
@@ -25,7 +25,7 @@ const Withdraw_returnModel = {
     },
     updatWithdraw_returnSasWwrid(req) {
         return new Promise((resolve, reject) => {
-            let insertQuery = "UPDATE withdraw_return SET withdraw_amount = ? WHERE withdraw_return_id = ?";
+            let insertQuery = "UPDATE withdraw_return SET withdraw_amount = ? , approve_status = ? WHERE withdraw_return_id = ?";
             let query = mysql.format(insertQuery, [req.amount, req.withdraw_return_id])
             connection().query(query, (err, result) => {
                 if (err) throw err
